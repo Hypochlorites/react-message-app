f5b2c61f957f6bea84dd26669f15e4c2ff15e546
Hypochlorites <sachinsaimisra@gmail.com>
2024-10-19 20:23:41 -0400

    Setup Message component to display ProfileBar when pfp or username is clicked. Not tested yet.

--- src/components/ChatComponents/Message.jsx
+++ src/components/ChatComponents/Message.jsx
@@ -1,15 +1,27 @@
+//React imports
+import { useState} from 'react'
+//Component imports
+import ProfileBar from '../ProfileComponents/ProfileBar'
+
+
 export default function Message({message, isIncoming, timestamp, otherUserPhoto, otherUsername, user, userPhoto}) {
+  //State Variables 
+  const [showBar, setShowBar] = useState(false)
+
+
+  //HTML
   return (
     <div className={`rounded-lg w-fit mt-4 p-2 ${!isIncoming? `bg-green-300 items-end ml-auto mr-2` : `bg-blue-200 items-start ml-2`}`}>
       <div className="flex">
-          <img className="rounded-full mr-2" src={isIncoming? otherUserPhoto : userPhoto } width="50px" height="50px"></img>
+          <img onClick={() => setShowBar(true)} className="rounded-full mr-2" src={isIncoming? otherUserPhoto : userPhoto } width="50px" height="50px"></img>
         <div className="flex flex-col">
           <div className="flex items-center">
-            {isIncoming ? (<p className="mr-2 font-semibold text-lg">{otherUsername}</p>) : (<p className="mr-2 font-semibold text-lg">{user}</p>) }
+            {isIncoming ? (<p onClick={() => setShowBar(true)} className="mr-2 font-semibold text-lg">{otherUsername}</p>) : (<p className="mr-2 font-semibold text-lg">{user}</p>) }
             <p className="text-xs">{timestamp}</p>
           </div>
           <p>{message}</p>
         </div>
+        {showBar && <ProfileBar setShowBar={setShowBar}/>}
       </div>
     </div>
   )
--- src/components/ProfileComponents/ProfileBar.jsx
+++ src/components/ProfileComponents/ProfileBar.jsx
@@ -2,20 +2,22 @@
 import { useCurrentUser } from '../contexts/CurrentUserContext'
 
 
-export default function ProfileBar() {  
+export default function ProfileBar({ setShowBar }) {  
   //Setup
   const { currentUser, currentUserObj } = useCurrentUser()
 
   
   //HTML
   return (
-    <div className="flex flex-col text-white bg-black items-center rounded-xl">
+    <div className="fixed inset-0 bg-opacity-50 z-50
+    flex flex-col text-white bg-black items-center rounded-xl">
       <div className="flex mt-2 items-center w-4/6 justify-between">
         <img src={currentUser.photoURL} width="200px" height="200px"></img>
         <h1 className="font-bold text-4xl"> {currentUser.displayName} </h1>
       </div>
       <p className="text-3xl font-semibold mt-4"> {currentUser.email} </p>
       <p className="text-2xl bg-gray-400 w-10/12 rounded-lg text-center mt-4 mb-4"> {currentUserObj.bio} </p>
+      <p onClick={() => setShowBar(false)}>X</p>
     </div>
   )
 }
