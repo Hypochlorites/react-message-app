4d2ffa4373657c6886fab25f2dd57fb2b42544b2
Hypochlorites <sachinsaimisra@gmail.com>
2024-11-27 18:36:05 -0500

    Use of textarea instead of <p> makes bar display longer bios better, preventing other elements from being pushed off the bar.

--- src/components/ProfileComponents/ProfileBar.jsx
+++ src/components/ProfileComponents/ProfileBar.jsx
@@ -10,8 +10,14 @@ export default function ProfileBar({ setShowProfileBar, position, photoURL, user
         <img className="rounded-full" src={photoURL} width="200px" height="200px"></img>
         <h1 className="font-bold text-4xl"> {username} </h1>
       </div>
-      <p className="text-2xl bg-gray-400 w-10/12 rounded-lg text-center mt-4 mb-4"> {bio} </p>
-      <button className="text-4xl bg-gray-500 border-2 rounded-lg border-white ml-2 mr-2 w-16 hover:bg-red-400" onClick={() => setShowProfileBar(false)}>X</button>
+      <textarea
+        className="resize-none text-2xl bg-gray-400 rounded-lg text-center mt-4 mb-4 min-h-60 w-11/12"
+        id="bio"
+        wrap="soft"
+        readOnly
+        value={bio}>
+      </textarea>
+      <button className="text-4xl bg-gray-500 border-2 rounded-lg border-white ml-2 mr-2 mb-2 w-16 hover:bg-red-400" onClick={() => setShowProfileBar(false)}>X</button>
     </div>
   )
 }
